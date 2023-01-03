2b47ba09de2c5a5caebf88c15c7ce735a534c5d1
Hernan Valencia <frjr17@criptext.com>
2023-01-03 15:40:34 -0500

    012 ngfor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,36 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl:'./app.component.html',
-  styleUrls:['./app.component.css'],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Luis'
-  imgURL = "https://picsum.photos/id/237/500/500"
+  name = 'luis ramirez';
+  imgURL = 'https://picsum.photos/id/237/500/500';
+  images = [
+    'https://picsum.photos/id/237/500/500',
+    'https://picsum.photos/id/237/500/500',
+    'https://picsum.photos/id/237/500/500',
+  ];
+  currentDate = new Date();
+  cost = 2000;
+  temperature = 25.3;
+  pizza = {
+    toppings: ['pepperoni', 'bacon'],
+    size: 'large',
+  };
+  blueClass = false;
+  fontSize = 16;
 
-  getName(){
-    return this.name
+  getName() {
+    return this.name;
   }
 
-  changeImage(e:KeyboardEvent){
-    this.imgURL = (e.target as HTMLInputElement).value
+  changeImage(e: KeyboardEvent) {
+    this.imgURL = (e.target as HTMLInputElement).value;
+  }
+
+  logImage(event: string) {
+    console.log(event);
   }
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,11 +1,66 @@
-import { Component,Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  EventEmitter,
+  Output,
+  OnInit,
+  OnChanges,
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewInit,
+  AfterViewChecked,
+  DoCheck,
+  OnDestroy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
 })
-export class PostComponent {
+export class PostComponent implements OnInit,OnChanges,
+AfterContentChecked,
+AfterContentInit,
+AfterViewInit,
+AfterViewChecked,
+DoCheck,OnDestroy {
+  @Input('img') postImg = '';
+  @Output() imgSelected = new EventEmitter<string>();
+
+  constructor() {
+    console.log('constructor() called', this.postImg);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit() called', this.postImg);
+  }
+
+  ngOnChanges() {
+    console.log('ngOnChanges() called');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck() called');
+  }
+
   
-  @Input() postImg = '';
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked() called');
+  }
+
+  ngAfterContentInit(): void {
+     console.log('ngAfterContentInit() called');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit() called');
+  }
+  
+  ngAfterViewChecked(): void {
+     console.log('ngAfterViewChecked() called');
+    }
+    ngOnDestroy(): void {
+      console.log('ngOnDestroy() called');
+      
+  }
 }
