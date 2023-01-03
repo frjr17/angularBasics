784af5dda100c898282f1370ae115d40dc51df7b
Hernan Valencia <frjr17@criptext.com>
2023-01-03 11:23:35 -0500

    006 Custom props

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,15 @@ import { Component } from '@angular/core';
   templateUrl:'./app.component.html',
   styleUrls:['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  name = 'Luis'
+  imgURL = "https://picsum.photos/id/237/500/500"
+
+  getName(){
+    return this.name
+  }
+
+  changeImage(e:KeyboardEvent){
+    this.imgURL = (e.target as HTMLInputElement).value
+  }
+}
